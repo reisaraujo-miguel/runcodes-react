8ceb0bddacffdb431fe56fe8189172abb08ae2f3
reisaraujo-miguel <reisaraujo.miguel@gmail.com>
2025-09-07 15:46:35 -0300

    make login page responsive to vertical screens

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -14,9 +14,9 @@ export function LoginPage() {
 
   return (
     <div className="min-h-screen bg-background">
-      <div className="container relative min-h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="relative min-h-screen flex flex-col lg:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+        {/* Desktop: Left Side (Logo, Theme Toggle, About Section) */}
         <div className="relative hidden h-full flex-col bg-muted p-10 lg:flex dark:border-r">
-          <div className="absolute inset-0" />
           <div className="relative z-20 flex items-center text-lg font-medium">
             <img
               src={
@@ -35,10 +35,38 @@ export function LoginPage() {
             <AboutSection />
           </div>
         </div>
-        <div className="flex items-center justify-center p-8 lg:p-0">
-          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+
+        {/* Desktop: Right Side (Login Card) */}
+        <div className="hidden lg:flex items-center justify-center p-8 lg:p-0">
+          <div className="mx-auto flex w-full flex-col items-center justify-center space-y-6 sm:w-[350px]">
             <LoginCard />
           </div>
+        </div>
+
+        {/* Mobile: Logo and Theme Toggle */}
+        <div className="lg:hidden flex items-center justify-between p-6">
+          <img
+            src={
+              isDarkTheme
+                ? "/src/assets/runcodes-logo/logo.png"
+                : "/src/assets/runcodes-logo/logoblue.png"
+            }
+            alt="RunCodes Logo"
+            className="h-10"
+          />
+          <ModeToggle />
+        </div>
+
+        {/* Mobile: Login Card */}
+        <div className="flex flex-1 items-center justify-center p-8 lg:hidden">
+          <div className="mx-auto flex w-full flex-col items-center justify-center space-y-6 sm:w-[350px]">
+            <LoginCard />
+          </div>
+        </div>
+
+        {/* Mobile: About Section */}
+        <div className="lg:hidden bg-muted p-6 dark:border-t">
+          <AboutSection />
         </div>
       </div>
     </div>
